aacd396cc4c93deb25eb0f4e044a8350599db6fd
vadim <gabderahmanov99@gmail.com>
2017-12-25 19:44:50 +0400

    add classes

--- src/com/math/volumeofbodies/Run.java
+++ src/com/math/volumeofbodies/Run.java
@@ -1,10 +1,15 @@
 package com.math.volumeofbodies;
 
+import javax.swing.JFrame;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame();
+		frame.setSize(300, 300);
+		frame.setLocation(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
---
+++ src/com/math/volumeofbodies/Volumes.java
@@ -0,0 +1,5 @@
+package com.math.volumeofbodies;
+
+public class Volumes {
+
+}
