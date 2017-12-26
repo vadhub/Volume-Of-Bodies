42d64f0e73830583ce3c121623af260ecad26273
vadim <gabderahmanov99@gmail.com>
2017-12-26 19:24:39 +0400

    add methods

--- src/com/math/volumeofbodies/Volumes.java
+++ src/com/math/volumeofbodies/Volumes.java
@@ -1,5 +1,16 @@
 package com.math.volumeofbodies;
 
 public class Volumes {
-
+	public double cube (double a){				
+		return a*a*a;
+	}
+	
+	public double parallelepiped (double a, double b, double h){				
+		return a*b*h;
+	}
+	
+	public double prism3 (double a,double b, double h){
+		double s = a*b;
+		return s*h;
+	}
 }
