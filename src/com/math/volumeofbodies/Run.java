c8dc880e2433954152027ddcea5ace3b893d02d0
vadim <gabderahmanov99@gmail.com>
2017-12-28 21:22:04 +0400

    checkstyle

--- src/com/math/volumeofbodies/Run.java
+++ src/com/math/volumeofbodies/Run.java
@@ -2,7 +2,6 @@ package com.math.volumeofbodies;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,35 +13,34 @@ public class Run {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		
+
 		JPanel tPanel = new JPanel(new FlowLayout());
 		JPanel bPanel = new JPanel(new FlowLayout());
-		
+
 		JButton btn1 = new JButton("ok");
-		
+
 		frame.setSize(300, 300);
 		frame.setLocation(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 		JTextField inText1= new JTextField(4);
 		JTextField inText2 = new JTextField(4);
 		JTextField inText3 = new JTextField(4);
-		
+
 		JTextField outText = new JTextField(4);
-		
+
 		tPanel.add(inText1);
 		tPanel.add(inText2);
 		tPanel.add(inText3);
-		
+
 		bPanel.add(btn1);
-		
-		mainPanel.add("South", bPanel);	
-		mainPanel.add("North", tPanel);	
-		
-		frame.add(mainPanel);	
-		
-				
+
+		mainPanel.add("South", bPanel);
+		mainPanel.add("North", tPanel);
+
+		frame.add(mainPanel);
+
 	}
 
 }
