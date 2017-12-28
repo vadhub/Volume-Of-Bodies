d32e0b9215ff4e7df4bad486665fae52d61e0b4a
vadim <gabderahmanov99@gmail.com>
2017-12-28 21:20:13 +0400

    add gui

--- src/com/math/volumeofbodies/Run.java
+++ src/com/math/volumeofbodies/Run.java
@@ -1,12 +1,25 @@
 package com.math.volumeofbodies;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Run {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		
+		JPanel tPanel = new JPanel(new FlowLayout());
+		JPanel bPanel = new JPanel(new FlowLayout());
+		
+		JButton btn1 = new JButton("ok");
+		
 		frame.setSize(300, 300);
 		frame.setLocation(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +28,21 @@ public class Run {
 		JTextField inText1= new JTextField(4);
 		JTextField inText2 = new JTextField(4);
 		JTextField inText3 = new JTextField(4);
+		
+		JTextField outText = new JTextField(4);
+		
+		tPanel.add(inText1);
+		tPanel.add(inText2);
+		tPanel.add(inText3);
+		
+		bPanel.add(btn1);
+		
+		mainPanel.add("South", bPanel);	
+		mainPanel.add("North", tPanel);	
+		
+		frame.add(mainPanel);	
+		
+				
 	}
 
 }
