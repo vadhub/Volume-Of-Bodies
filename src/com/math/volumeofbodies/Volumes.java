9b4cfc249f0817a83c983e6dbb4a0509a495ec16
vadim <gabderahmanov99@gmail.com>
2017-12-30 19:26:33 +0400

    interface

--- src/com/math/volumeofbodies/Volumes.java
+++ src/com/math/volumeofbodies/Volumes.java
@@ -1,16 +1,31 @@
 package com.math.volumeofbodies;
 
-public class Volumes {
-	public double cube (double a){				
+public class Volumes implements IVolumes {
+
+	@Override
+	public double cube(double a) {
 		return a*a*a;
 	}
-	
-	public double parallelepiped (double a, double b, double h){				
+
+	@Override
+	public double aprallelepiped(double a, double b, double h) {
 		return a*b*h;
 	}
-	
-	public double prism3 (double a,double b, double h){
-		double s = a*b;
-		return s*h;
+
+	@Override
+	public double prism() {
+		return 0;
 	}
+	// public double cube (double a){
+	// return a*a*a;
+	// }
+	//
+	// public double aprallelepiped (double a, double b, double h){
+	// return a*b*h;
+	// }
+	//
+	// public double prism3 (double a,double b, double h){
+	// double s = a*b;
+	// return s*h;
+	// }
 }
