4e1affdda23e63f1e49b342a5562de6746c9ace2
vadim <gabderahmanov99@gmail.com>
2018-01-27 18:18:44 +0400

    add getter & setter

--- src/com/volume/figure/Prism.java
+++ src/com/volume/figure/Prism.java
@@ -6,10 +6,20 @@ import com.math.volumeofbodies.IVolume;
 public class Prism extends AbstractFigure implements IVolume{
 	
 	private static double area;
+	
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		Prism.area = area;
+	}
 
 	public Prism(double sideA, double sideB, double hight, double angle,double diagonal, double areas) {
 		super(sideA, sideB, hight, angle, diagonal, areas);		
-	}	
+	}
+	
+	
 
 	@Override
 	public double volume(double h, double area) {
@@ -18,14 +28,6 @@ public class Prism extends AbstractFigure implements IVolume{
 		this.setArea(area);
 		
 		return area*h;
-	}
-
-	public static double getArea() {
-		return area;
-	}
-
-	public void setArea(double area) {
-		Prism.area = area;
-	}
+	}	
 	
 }
--- src/com/volume/figure/Pyramid.java
+++ src/com/volume/figure/Pyramid.java
@@ -1,24 +1,31 @@
 package com.volume.figure;
 
 import com.math.volumeofbodies.AbstractFigure;
-import com.math.volumeofbodies.IFigureArea;
 import com.math.volumeofbodies.IVolume;
 
-public class Pyramid extends AbstractFigure implements IVolume, IFigureArea{
+public class Pyramid extends AbstractFigure implements IVolume{
+	
+	private static double area;
+	
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		Pyramid.area = area;
+	}
 
 	public Pyramid(double sideA, double sideB, double hight, double angle, double diagonal, double areas) {
 		super(sideA, sideB, hight, angle, diagonal, areas);	
 	}
-
-	@Override
-	public double area(double a, double b, double h, double angle,double diagonal) {
-		return 0;
-	}
+	
 
 	@Override
 	public double volume(double h, double area) {
 		h = hight;
-		return (area(sideA,sideB,0,0,0)*h)/3;
-	}
+		this.setArea(area);
+		return (area*h)/3;
+	}	
+	
 
 }
